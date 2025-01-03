81cf1487a1550f678985a9fe2d4f061392c8c860
Taufique-Ansari <Taufiqueansari451@mail.com>
2025-01-03 17:24:29 +0530

    Remove prototype_2 git repo

---
+++ prototype_2/next.config.js
@@ -0,0 +1,13 @@
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+        },
+      ],
+    },
+    output: 'standalone',
+  };
+  
+  module.exports = nextConfig;
